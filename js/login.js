7a0c2e193133464731684a9da296d5564ed5ef63
saurabh <saurabh6790@gmail.com>
2015-06-19 22:13:33 +0530

    [POS.htnl] and menifest

--- js/login.js
+++ js/login.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 	$('.form-signin').on("submit", function(event){
-		
+		usr = $("#user").val();
+		pwd = $('#inputPassword').val();
+		$.ajax({
+			url: 'http://test_phr/api/method/login',
+			method: "POST",
+			async: false,
+			data:{"usr":usr, "pwd":pwd},
+			dataType: "json",
+			success: function(r) {
+				window.location.href = "http://spos/pages/pos.html";
+			},
+			error: function(XMLHttpRequest, textStatus, errorThrown) {
+				console.log(textStatus);
+			}
+		});
 	})
 })

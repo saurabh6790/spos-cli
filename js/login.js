b08b59aa80909d592b0936104642de4001d14065
saurabh <saurabh6790@gmail.com>
2015-06-22 17:03:26 +0530

    [Feature] login/logout process

--- js/login.js
+++ js/login.js
@@ -1,23 +1,39 @@
 $(document).ready(function(){
-	// $('.form-signin').on("submit", function(event){
-	// 	usr = $("#user").val();
-	// 	pwd = $('#inputPassword').val();
-	// 	$.ajax({
-	// 		url: 'http://test_phr/api/method/login',
-	// 		method: "POST",
-	// 		async: false,
-	// 		data:{"usr":usr, "pwd":pwd},
-	// 		dataType: "json",
-	// 		success: function(r) {
-	// 			window.location.href = "http://spos/pages/pos.html";
-	// 		},
-	// 		error: function(XMLHttpRequest, textStatus, errorThrown) {
-	// 			console.log(textStatus);
-	// 		}
-	// 	});	
-	// })
-
-	$('#signin').click(function(){
-		window.location = "./pos.html"
-	})
+	bind_login_event()	
 })
+
+function bind_login_event(){
+	var args = {};
+	$('#signin').click(function(){
+		args.cmd = "login";
+		args.usr = ($("#inputUser").val() || "").trim();
+		args.pwd = $("#inputPassword").val();
+		args.domain = $("#inputhDomain").val();
+
+		$('.btn-primary').prop("disabled", true);
+		if(!args.usr || !args.pwd || !args.domain)  {
+			alert("Domain, Login and Password required");
+			return false;
+		}
+		login(args)
+	})	
+}
+
+function login(args){
+	$.ajax({
+		type: "POST",
+		url: "http://"+args['domain']+"/api/method/login?usr="+args['usr']+"&pwd="+args['pwd'],
+		dataType: "json",
+		success: function(r) {
+			console.log(r)
+			window.location = "./pages/pos.html";
+			$.jStorage.set("user", r.full_name)
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			alert("Invalid Login")
+			window.location = "../"
+		}
+	}).always(function(){
+		$('.btn-primary').prop("disabled", false);
+	})
+}

24f661151b1859e0549d35bb56a488a1760cbde9
suyash <suyash.p@indictranstech.com>
2015-08-06 11:25:45 +0530

    [Feature]: Auto-Pop_up on item selection from scanner

--- js/pos.js
+++ js/pos.js
@@ -182,25 +182,38 @@ $(document).ready(function(){
 
 
     $("[name=item][type=text]").change(function(){
-        item_dict = $.grep($.jStorage.get("item"), function(e){ return e.item_code == $("[name=item][type=text]").val(); });        
-        if(item_dict.length){
-          render_thumbnails(item_dict)
-        }         
-        else if(!$(this).val()){
-           check_for_render_thumbnails()
+        return_flag = validate_for_vendor_selection_on_item_selection()
+        if (return_flag){
+          item_dict = $.grep($.jStorage.get("item"), function(e){ return e.item_code == $("[name=item][type=text]").val(); });
+          if(item_dict.length){
+            render_thumbnails(item_dict)
+          }
+          else if(!$(this).val()){
+             check_for_render_thumbnails()
+          }
         }
 
     })
 
+    $("[name=item][type=text]").scannerDetection(function(){
+        return_flag = validate_for_vendor_selection_on_item_selection()
+        if (return_flag){
+          item_dict = $.grep($.jStorage.get("item"), function(e){ return e.item_code == $("[name=item][type=text]").val(); });        
+          if(item_dict.length){
+            $(".thumbnail").trigger("click")
+          }     
+        }
+     })
+
       $("[name=item][type=text]").keypress(function(){
-         validate_for_vendor_selection_on_item_selection()
-         execute_item_search_span_trigger()
-         
+        return_flag = validate_for_vendor_selection_on_item_selection()
+        if (return_flag){
+          execute_item_search_span_trigger()  
+        }     
       })
  
       $("[name=sub_category][type=text]").keypress(function(){
-         execute_sub_category_search_span_trigger()
-         
+        execute_sub_category_search_span_trigger()         
       })
 
     $("#submit_order").click(function(){
@@ -693,8 +706,9 @@ function validate_for_vendor_selection_on_item_selection(){
 
     if(!$("[name=vendor][type=text]").val()){
         show_message('Please Select Vendor for Item Selection',"Mandatory Field")
+        return false
     }
-
+    return true
 }
 
 
